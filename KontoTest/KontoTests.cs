b95e3d4651f541db48f9404dbcb213e28efcee6b
[DZardasht <zardasht.z2@gso.schule.koeln>
2022-11-28 10:19:23 +0100

    EinzahlenTest versuch 1

---
+++ KontoTest/KontoTests.cs
@@ -0,0 +1,22 @@
+using Bank;
+
+namespace KontoTest
+{
+    [TestClass]
+    public class KontoTests
+    {
+        [TestMethod]
+        public void Einzahlen()
+        {
+            //Arrange
+            Konto k1 = new Konto(100);
+
+            //Act
+            int guthaben = 20;
+            guthaben = guthaben + k1.Guthaben;
+            k1.Einzahlen(20);
+            //Assert
+            Assert.AreEqual(k1.Guthaben, guthaben);
+        }
+    }
+}
